6047211db3ace7f03c48d979e516d6fcf046c6c5
Somsubhra Das <das.somsubhra1@gmail.com>
2018-11-11 23:42:37 +0530

    Added build files

---
+++ docs/precache-manifest.b9ec5e9f990a7a7a71c27526621ab3ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2e79577bcff75752c842",
+    "url": "./static/js/main.2e79577b.chunk.js"
+  },
+  {
+    "revision": "e950da1575fbb0a6f92e",
+    "url": "./static/js/1.e950da15.chunk.js"
+  },
+  {
+    "revision": "2e79577bcff75752c842",
+    "url": "./static/css/main.c715293f.chunk.css"
+  },
+  {
+    "revision": "f967ba8c369365ef62cb97e75a87fe5b",
+    "url": "./index.html"
+  }
+];
